99cab67120fa71137796195cbdd61eefc4745b54
AsHansen88 <Ande970s@stud.kea.dk>
2022-08-30 19:26:47 +0200

    HEllo world

---
+++ src/test/java/com/example/springguide/SpringguideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringguideApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
